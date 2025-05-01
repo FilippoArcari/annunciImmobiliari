8b6708529624a5f3574d317859ebf9657cb5697c
FilippoArcari <filippoarcari2004@gmail.com>
2025-05-01 21:29:16 +0200

    refactor: improve parameter handling in calendar event retrieval

--- app/api/calendar/[id]/route.ts
+++ app/api/calendar/[id]/route.ts
@@ -5,12 +5,10 @@ interface CalendarEvent {
 
 export async function GET(
 	request: Request,
-	{ params }: { params: { id: string } }
+	context: { params: { id: string } }
 ) {
-	// Await params
-	const { id } = await params;
+	const { id } = context.params;
 
-	// Validate that 'id' exists
 	if (!id) {
 		return new Response(JSON.stringify({ error: "ID parameter is missing" }), {
 			status: 400,
@@ -19,18 +17,16 @@ export async function GET(
 
 	const calendarId = process.env.CALENDAR_ID!;
 	const apiKey = process.env.GOOGLE_CALENDAR_API_KEY!;
-	const eventId = id;
-
 	const url = `https://www.googleapis.com/calendar/v3/calendars/${calendarId}/events?key=${apiKey}`;
 	const res = await fetch(url);
 	const data = await res.json();
-	
-	// filtro per id: X nel campo description TODO:quando aggiundo l'evento lo metto nella descrizione id:<numero>
+
 	const filtered = data.items.filter((event: CalendarEvent) =>
-		event.description?.includes(`id:${eventId}`)
+		event.description?.includes(`id:${id}`)
 	);
 
 	return Response.json(filtered, {
 		status: 200,
 	});
 }
+
