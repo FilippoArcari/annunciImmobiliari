efb2cc64d7eef1efc644baa79a142101c72c9364
DanielCasa02 <daniel.casagranda.02@gmail.com>
2025-05-05 22:44:16 +0200

    aggiunta descrizoni e video

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,19 +1,19 @@
 export async function GET() {
     const data = {
 			anagrafiche: {
-				nome: "Mario",
-				cognome: "Rossi",
-				mail: "prova@25aprile.com",
+				nome: "Daniel",
+				cognome: "Casagranda",
+				mail: "bighouseserviceit@gmail.com",
 			},
 			immobili: [
 				{
 					id: 0,
-					titolo: "Appartamento a Pinè",
-					descrizione: "Appartamento luminoso e spazioso in centro città.",
+					titolo: "Appartamento N°1",
+					descrizione: "Traumhaftes Ferienhaus mit atemberaubendem Gardasee-Blick  Perfekter Urlaub in Tignale!Genießen Sie einen unvergesslichen Aufenthalt in diesem charmanten 50 m² großen Apartment mit 4 Betten und 4 Räumen, nur 5 Gehminuten vom Zentrum Tignales entfernt. Das Highlight? Ein weiter Garten mit atemberaubendem Panoramablick über den gesamten Gardasee  ideal zum Entspannen und für romantische Abende unter dem italienischen Himmel.",
 					copertina: "/app_1_03.jpg",
-					affitto: "700€",
-					superficie: "80",
-					locali: "3",
+					affitto: "60€/day",
+					superficie: "50 metri quadrati",
+					locali: "4",
 					bagni: "1",
 					immagini: [
 						"/app_1_01.jpg",
@@ -25,7 +25,7 @@ export async function GET() {
 						"/app_1_07.jpg",
 						"/app_1_08.jpg",
 					],
-					video: ["/app_1_01.mp4"],
+					video: ["/app_1_01.mp4","/app_1_02.mp4"],
 					coordinates: {
 						lat: 45.739054,
 						lng: 10.713260,
@@ -33,13 +33,13 @@ export async function GET() {
 				},
 				{
 					id: 1,
-					titolo: "Appartamento in fuori",
-					descrizione: "Descrizione dell'appartamento in fuori",
+					titolo: "Appartamento N°2",
+					descrizione: "Traumhaftes Ferienhaus mit atemberaubendem Gardasee-Blick  Perfekter Urlaub in Tignale!Genießen Sie einen unvergesslichen Aufenthalt in diesem charmanten 50 m² großen Apartment mit 4 Betten und 4 Räumen, nur 5 Gehminuten vom Zentrum Tignales entfernt. Das Highlight? Ein weiter Garten mit atemberaubendem Panoramablick über den gesamten Gardasee  ideal zum Entspannen und für romantische Abende unter dem italienischen Himmel.",
 					copertina: "/app_2_02.jpg",
-					affitto: "500€",
-					superficie: "20",
-					locali: "3",
-					bagni: "2",
+					affitto: "60€",
+					superficie: "50 metri quadri",
+					locali: "4",
+					bagni: "1",
 					immagini: [
 						"/app_2_01.jpg",
 						"/app_2_02.jpg",
